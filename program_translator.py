e7930fa915a86993cd0815f0ab30fb1350f3b24c
Carlos Ferrús Ferri <cmff1@alu.ua.es>
2020-11-13 00:09:46 +0100

    Translator now trained from corpus

--- program_translator.py
+++ program_translator.py
@@ -28,8 +28,22 @@ def JSON2RNN(json):
         res.append(JSON2RNN_recurs(j))
     return res
 
-def RNN2JSON(rnn):
-    return str(rnn)
+#def RNN2JSON(rnn):
+    #return str(rnn)
+    
+def train_tokenizer(tokenizer):
+    with open("config.json", 'r') as fp:
+        CONFIG = json.load(fp)
+        fp.close()
+    CONFIG = CONFIG['DEFAULT']
+    tokenizer.fit_on_texts(CONFIG['methods'].values())
+    tokenizer.fit_on_texts(CONFIG['t'])
+    tokenizer.fit_on_texts(CONFIG['s'])
+    positives = [str(i) for i in range(CONFIG['MAX_STR_SIZE'])]
+    negatives = [str(-i - 1) for i in range(CONFIG['MAX_STR_SIZE'])]
+    tokenizer.fit_on_texts(positives)
+    tokenizer.fit_on_texts(negatives)
+    return tokenizer
 
 def str2bool(v):
     if isinstance(v, bool):
@@ -40,6 +54,7 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Program translator JSON - Number Array")
@@ -48,30 +63,30 @@ if __name__ == "__main__":
     parser.add_argument('JSONtoRNN', type=str2bool, help='True if input file is JSON, False if input file is number array (aka RNN)')
     args = parser.parse_args()
     
+    tokenizer = Tokenizer(filters='', lower=False, oov_token="_")
+    tokenizer = train_tokenizer(tokenizer)
+    
     with open(args.file, 'r') as f:
-        if args.JSONtoRNN:
-            inpt = json.load(f)
-        else:
-            inpt = f.readline()
+        inpt = json.load(f)
         f.close()
         
-    if not args.JSONtoRNN:
-        inpt = inpt.split()
-        inpt = [int(i) for i in inpt]
+    #if not args.JSONtoRNN:
+        #inpt = inpt.split()
+        #inpt = [int(i) for i in inpt]
         
     if args.JSONtoRNN:
         res = JSON2RNN(inpt)
         print(res)
-        tokenizer = Tokenizer(filters='')
-        tokenizer.fit_on_texts(res)
         res = tokenizer.texts_to_sequences(res)
     else:
-        res = RNN2JSON(inpt)
+        #res = RNN2JSON(inpt)
+        print(inpt)
+        res = tokenizer.sequences_to_texts(inpt)
     
     
     if args.save is not None:
         with open(args.save, 'w') as f:
-            f.write(res)
+            json.dump(res, f)
             f.close()
     
     print(res)
