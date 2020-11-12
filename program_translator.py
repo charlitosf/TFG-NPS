e756a2056417b9113768b9c7be219b8b013a780e
Carlos Ferrús Ferri <cmff1@alu.ua.es>
2020-11-12 11:50:12 +0100

    Program translator started

---
+++ program_translator.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 12 11:01:40 2020
+
+@author: charl
+"""
+import argparse
+import json
+
+def JSON2RNN(json):
+    res = []
+    if isinstance(json, dict):
+        for key in json.keys():
+            res.append(str(key))
+            res += JSON2RNN(json[key])
+    elif isinstance(json, list):
+        for el in json:
+            res += JSON2RNN(el)
+    else:
+        res.append(str(json))
+    return res
+
+def RNN2JSON(rnn):
+    return str(rnn)
+
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Program translator JSON - Number Array")
+    parser.add_argument('-s', '--save', metavar='FILE', help='Path where the output will be stored')
+    parser.add_argument('file', help='Path to input file to be translated')
+    parser.add_argument('JSONtoRNN', type=str2bool, help='True if input file is JSON, False if input file is number array (aka RNN)')
+    args = parser.parse_args()
+    
+    with open(args.file, 'r') as f:
+        if args.JSONtoRNN:
+            inpt = json.load(f)
+        else:
+            inpt = f.readline()
+        f.close()
+    if not args.JSONtoRNN:
+        inpt = inpt.split()
+        inpt = [int(i) for i in inpt]
+        
+    if args.JSONtoRNN:
+        res = JSON2RNN(inpt)
+    else:
+        res = RNN2JSON(inpt)
+    
+    
+    if args.save is not None:
+        with open(args.save, 'w') as f:
+            f.write(res)
+            f.close()
+    
+    print(res)
